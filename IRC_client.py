a293bbf436d5835fdf58cd04a92ad21e4f1eaee8
Dannnno <dannnno16@gmail.com>
2014-09-30 02:08:59 -0700

    Cleaner GUI
    - Got it looking more how I wanted
    - Got kvlang working
    - Included an image
    - Needs to start getting some business logic

--- IRC_client.py
+++ IRC_client.py
@@ -9,40 +9,22 @@ from kivy.uix.tabbedpanel import TabbedPanel, TabbedPanelItem
 from kivy.uix.widget import Widget
 
 
-
 class IRC_Widget(Widget): pass
 
+class Chat_Window(TabbedPanel): pass
 
-class IRC_Client(App): 
+class Channel(TabbedPanelItem): pass
+
+class User_List(Label): pass
+
+class Text_Entry(BoxLayout): pass
+
+class Menu_Bar(BoxLayout): pass
+
+class IRC_ClientApp(App): 
     
     def build(self):
-        big_layout = BoxLayout(orientation="vertical")
-        filebar = BoxLayout(orientation="horizontal")
-        filebar.size_hint = (1, 0.1)
-        filebutton = Button(text="File")
-        setbutton = Button(text="Settings")
-        helpbutton = Button(text="Help")
-        filebar.add_widget(filebutton)
-        filebar.add_widget(setbutton)
-        filebar.add_widget(helpbutton)
-        sidebar = Label(text="This will be where you can pick from your servers and channels")
-        sidebar.size_hint = (.3, 1)
-        body = BoxLayout(orientation="horizontal")
-        chat_area = BoxLayout(orientation="vertical")
-        users = Label(text="This will be where you see the users!")
-        users.size_hint = (.3, 1)
-        chat = Label(text="This is where you see messages")
-        chat.size_hint = (1, .9)
-        entry = Label(text="This is where you type")
-        entry.size_hint = (1, .1)
-        chat_area.add_widget(chat)
-        chat_area.add_widget(entry)
-        body.add_widget(sidebar)
-        body.add_widget(chat_area)
-        body.add_widget(users)
-        big_layout.add_widget(filebar)
-        big_layout.add_widget(body)
-        return big_layout
-        
+        return IRC_Widget()
+                
 if __name__ == "__main__":
-    IRC_Client().run()
+    IRC_ClientApp().run()
