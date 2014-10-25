94e49c72f4124ad8984c94bb723c534db48e1562
Dannnno <dannnno16@gmail.com>
2014-10-24 21:07:23 -0700

    Cleaned up how I load tests

---
+++ runtests.py
@@ -0,0 +1,43 @@
+"""
+Copyright (c) 2014 Dan Obermiller
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+You should have received a copy of the MIT License along with this program.
+If not, see <http://opensource.org/licenses/MIT>
+"""
+
+import inspect
+import Testing
+import sys
+import unittest
+
+loader = unittest.TestLoader()
+suites_list = []
+for key, module in inspect.getmembers(Testing):
+    for sub_key, aclass in inspect.getmembers(module):
+        try:
+            if issubclass(aclass, unittest.TestCase):
+                suites_list.append(loader.loadTestsFromTestCase(aclass))
+        except TypeError: continue
+            
+big_suite = unittest.TestSuite(suites_list)
+
+runner = unittest.TextTestRunner(sys.stdout, verbosity=1)
+runner.run(big_suite)
